b5503ca06cd8f84c14d4435587fdcee609149500
ꜱᴛᴀɪɴ     <evansken2008@gmail.com>
2026-04-18 09:56:24 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  BOT_TOKEN: process.env.BOT_TOKEN || "",
-  OWNER_ID: ["7259883138"],
-  MEMEK_ID: process.env.CHANNEL_ID || "3800137368",   // Your Telegram channel ID (numbers, not username)
-  KONTOL: [],                                // Extra owner IDs to receive /req messages
-};
-
-module.exports = config;
