0f4f4750991535ba500496af2ee509e2f4e6446f
ꜱᴛᴀɪɴ     <evansken2008@gmail.com>
2026-04-18 09:56:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   BOT_TOKEN: process.env.BOT_TOKEN || "",
   OWNER_ID: ["7259883138"],
-  MEMEK_ID: process.env.CHANNEL_ID || "",   // Your Telegram channel ID (numbers, not username)
+  MEMEK_ID: process.env.CHANNEL_ID || "3800137368",   // Your Telegram channel ID (numbers, not username)
   KONTOL: [],                                // Extra owner IDs to receive /req messages
 };
 
